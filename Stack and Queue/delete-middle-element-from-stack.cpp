b97d45ce85a56113e394df2c2abc1ec22ff7f46d
tirumalasri13 <tirumalasriampolu@gmail.com>
2026-02-12 13:57:08 +0530

    practice

--- Stack and Queue/delete-middle-element-from-stack.cpp
+++ Stack and Queue/delete-middle-element-from-stack.cpp
@@ -3,7 +3,7 @@ void deleteMiddle(stack<int>&inputStack, int N){
     int total = N + 1;
     
     if(total == 0) return;
-    int mid = (total - 1) / 2;              // from bottom (0-based)
+    int mid = (total - 1) / 2;             
     int pops = total - mid - 1; 
     stack<int> temp;
     for(int i = 0; i < mid; i++) {
