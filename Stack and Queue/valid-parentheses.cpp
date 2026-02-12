2db3dcc4d9b5c5a9b3f404a3224053fcec83a1b1
tirumalasri13 <tirumalasriampolu@gmail.com>
2026-02-12 15:29:06 +0530

    Practice

---
+++ Stack and Queue/valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if(st.empty()) return false;
+                if((ch == ')' && st.top() == '(') ||
+                   (ch == '}' && st.top() == '{') ||
+                   (ch == ']' && st.top() == '[')) {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();    
+    }
+};
