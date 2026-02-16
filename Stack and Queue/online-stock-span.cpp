b98857eaba4332e2536b3045592e0e1a58d6b279
tirumalasri13 <tirumalasriampolu@gmail.com>
2026-02-16 12:38:25 +0530

    Practice

---
+++ Stack and Queue/online-stock-span.cpp
@@ -0,0 +1,40 @@
+// Brute Force.....
+class StockSpanner {
+public:
+    vector<int> prices;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        prices.push_back(price);
+        int span=0;
+        for(int i=prices.size()-1; i>=0; i--){
+            if(prices[i]<=price) span++;
+            else break; 
+        }
+        return span;
+    }
+};
+
+//Monotonic Stack
+
+class StockSpanner {
+public:
+    vector<int> prices;
+    stack<int> st;
+    StockSpanner() {     
+    } 
+    int next(int price) {
+        prices.push_back(price);
+        int i = prices.size() - 1;
+        while(!st.empty() && prices[st.top()] <= price) {
+            st.pop();
+        }
+        int span;
+        if(st.empty()) span = i + 1;
+        else span = i - st.top();
+        st.push(i);
+        return span;
+    }
+};
