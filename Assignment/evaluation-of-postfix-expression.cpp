d990c52dba82393bc49ddc9323e516ebb1d5b71e
tirumalasri13 <tirumalasriampolu@gmail.com>
2026-02-16 09:57:02 +0530

    HomeWork

--- Assignment/evaluation-of-postfix-expression.cpp
+++ Assignment/evaluation-of-postfix-expression.cpp
@@ -27,3 +27,32 @@ class Solution {
         return st.top();
     }
 };
+
+class Solution {
+  public:
+    bool operators(char c) {
+        return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+    }
+    int evaluatePostfix(vector<string>& arr) {
+        // code here
+        int n = arr.size();
+        stack<double> st;
+        for (int i = 0; i < n; i++) {
+            if (arr[i].length() > 1 || isdigit(arr[i][0])) {
+                st.push(stoi(arr[i]));
+            }
+            else {
+                double op1 = st.top(); st.pop();
+                double op2 = st.top(); st.pop();
+                switch (arr[i][0]) {
+                    case '+': st.push(op2 + op1); break;
+                    case '-': st.push(op2 - op1); break;
+                    case '*': st.push(op2 * op1); break;
+                    case '/': st.push(floor(op2 / op1)); break;
+                    case '^': st.push(pow(op2, op1)); break;
+                }
+            }
+        }
+        return st.top();
+    }
+};
