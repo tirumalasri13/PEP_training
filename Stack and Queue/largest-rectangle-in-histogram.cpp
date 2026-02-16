5dabec1f92346149352d0953f69a5630225af933
tirumalasri13 <tirumalasriampolu@gmail.com>
2026-02-16 15:56:53 +0530

    Practice

--- Stack and Queue/largest-rectangle-in-histogram.cpp
+++ Stack and Queue/largest-rectangle-in-histogram.cpp
@@ -22,3 +22,40 @@ public:
         return maxheight;
     }
 };
+
+
+
+///NSL AND NSR..
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+        // NSL
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+        // NSR
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        long long maxArea = 0;
+        for(int i = 0; i < n; i++) {
+            long long width = right[i] - left[i] - 1;
+            maxArea = max(maxArea, width * heights[i]);
+        }
+        return (int)maxArea;
+    }
+};
