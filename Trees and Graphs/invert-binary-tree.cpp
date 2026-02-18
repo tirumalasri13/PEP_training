b0d1608945e9ab72f8207d9e67d378763904db8b
tirumalasri13 <tirumalasriampolu@gmail.com>
2026-02-18 11:55:48 +0530

    Practice

--- Trees and Graphs/invert-binary-tree.cpp
+++ Trees and Graphs/invert-binary-tree.cpp
@@ -10,12 +10,33 @@
  * };
  */
 class Solution {
-public:
+public:binary-tree-level-order-traversal
     TreeNode* invertTree(TreeNode* root) {
         if(root==NULL) return NULL;
         swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
+        return root;
+
+        
+    }
+};
+//BFS..
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            swap(node->left, node->right);
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
+
         return root;
 
         
